fe14e5d767da97534facf3f3df6b8a9c8dc09b21
rmcharg <r_mcharg@outlook.com>
2024-08-22 00:58:07 +0100

    Added buy functionality to webapp storing the transaction and updating the users cash balance

--- flaskr/helpers.py
+++ flaskr/helpers.py
@@ -1,5 +1,10 @@
 from flask import redirect, session
 from functools import wraps
+import yfinance as yf
+import sqlite3
+
+DATABASE_NAME = 'flaskr/trade.db'
+
 
 # Decorator for routes so that is login is required
 def login_required(orig_func):
@@ -13,4 +18,70 @@ def login_required(orig_func):
         return orig_func(*args, **kwargs)
     
     return decorated_function
+
+def get_user_cash(id):
+        """Get user cash from the database"""
+        conn = sqlite3.connect(DATABASE_NAME)
+        conn.row_factory = sqlite3.Row
+        cur = conn.cursor()
+        rows = cur.execute('SELECT cash FROM users WHERE id = ?',(session['user_id'],)).fetchall()
+        cash = rows[0]['cash']
+
+        return cash 
+
+def update_user_cash(id, cash):
+        conn = sqlite3.connect(DATABASE_NAME)
+        cur = conn.cursor()
+        cur.execute("UPDATE users SET cash = ? WHERE id = ?", (cash, id))
+        conn.commit()
+
+def get_stock(symbol):
+    try:
+        ticker = yf.Ticker(symbol)
+    except:
+        return None
+    
+    stock_data = ticker.history(period="1d", interval="1m")
+    current_price = stock_data.iloc[-1].Close
+    return {"price": current_price, "symbol": symbol}
+
+def get_user_portfolio(id):
+    conn = sqlite3.connect(DATABASE_NAME)
+    conn.row_factory = sqlite3.Row
+    cur = conn.cursor()
+    rows = cur.execute(
+        "SELECT symbol, SUM(shares) FROM transactions WHERE user_id = ? GROUP BY symbol"
+        , (id,)).fetchall()
+    
+    stocks = []
+    portfolio_value = 0
+    for row in rows:
+        stock = get_stock(row['symbol'])
+
+        # Add number of shares of the stock and the value of the shares
+        stock['shares'] = row['SUM(shares)']
+        stock['total'] = round(stock['price'] * stock['shares'], 2)
+        stock['price'] = round(stock['price'], 2)
+
+        portfolio_value += stock['total']
+
+        stocks.append(stock)
+    
+    cash = round(get_user_cash(id),2)
+    portfolio_value = round(portfolio_value, 2)
+
+    return {'stocks': stocks, 'cash': cash, 'value': portfolio_value}
+
+def add_transaction(id, symbol, shares, price, time, type):
+        conn = sqlite3.connect(DATABASE_NAME)
+        conn.row_factory = sqlite3.Row
+        cur = conn.cursor()
+
+        cur.execute(
+            "INSERT INTO transactions (user_id, symbol, shares, price, type, datetime) VALUES (?, ?, ?, ?, ?, ?)",
+            (id, symbol.upper(), shares, price,type,time))
+
+        conn.commit()
+    
+
     
--- flaskr/views.py
+++ flaskr/views.py
@@ -1,12 +1,16 @@
-from flask import Blueprint, render_template, request, session, jsonify
-from flaskr.helpers import login_required
+from flask import Blueprint, render_template, request, session, jsonify, flash, redirect
+from flaskr.helpers import login_required, get_stock, get_user_portfolio, get_user_cash, add_transaction
+from flaskr.helpers import update_user_cash
+from datetime import datetime
 import yfinance as yf
 import pandas as pd
 import time
+import sqlite3 
 
 views = Blueprint('views', __name__)
 
 TICKERS = ['AAPL', 'NFLX']
+DATABASE_NAME = 'flaskr/trade.db'
 
 @views.after_request
 def after_request(response):
@@ -16,24 +20,85 @@ def after_request(response):
     response.headers["Pragma"] = "no-cache"
     return response
 
+
 @views.route("/")
 @login_required
 def index():
     return render_template("index.html")
 
+
 @views.route("/portfolio")
 @login_required
 def portfolio():
+    return render_template("portfolio.html", portfolio=get_user_portfolio(session['user_id']))
+
+
+@views.route("/buy", methods=["GET", "POST"])
+@login_required
+def buy():
     
-    return render_template("portfolio.html")
+    if request.method == "GET":
+        return render_template("buy.html")
+    else:
+        symbol = request.form.get("symbol")
+
+        if symbol is None:
+            flash('Must enter a symbol for the stock you wish to buy!')
+            return redirect("/buy")
+        
+        try:
+            shares = int(request.form.get("shares"))
+        except:
+            flash('Invalid number of shares')
+            return redirect("/buy")
+        
+        # check that the shares is not empty and it is positive
+        if shares <= 0 or shares is None:
+            flash('Invalid number of shares')
+            return redirect("/buy")
+        
+        # try to get the stock value
+        stock = get_stock(symbol)
+        print(stock)
+        if stock is None:
+            flash('Invalid Symbol')
+            return redirect("/buy")
+
+        # connect to database to get user budget
+        cash = get_user_cash(id)
+        price = stock['price']
+        transaction_cost = price * shares
+
+        if cash - transaction_cost < 0:
+            flash('You do not have the cash for this purchase')
+            return redirect("/buy")
+        
+        # Get date and time (ISO 8601 format) and insert transaction record into db
+        time = datetime.now().strftime("%d-%m-%Y %H:%M:%S")
+        user_id = session["user_id"]
+        add_transaction(user_id, symbol, shares, price, time, 'Buy')
+
+        # Update cash in users table
+        new_cash = cash - transaction_cost
+        update_user_cash(session['user_id'],new_cash)
+
+        return redirect("/portfolio")
+
+
+@views.route('/sell')
+@login_required
+def sell():
+    return render_template("sell.html")
+
 
 @views.route("/transactions")
 @login_required
 def transactions():
     return render_template("transactions.html")
 
-@views.route('/get_data')
-def get_data():
+@views.route('/get_dashboard_data')
+@login_required
+def get_dashboard_data():
     data = []
     for ticker_symbol in TICKERS:
         ticker = yf.Ticker(ticker_symbol)
@@ -41,4 +106,7 @@ def get_data():
         data.append({'ticker': ticker_symbol, 'currentPrice': round(info['currentPrice'],2)})
 
     return jsonify(data)
+
+
+
         
